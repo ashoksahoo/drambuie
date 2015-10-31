9da37a4c17d9b7aeedc18423bac1f477d3e4cf6d
Ashok Sahoo <hello@ashok.io>
2015-10-31 20:42:53 +0530

    Added diff resposne for html and json.

--- utils/authResponse.js
+++ utils/authResponse.js
@@ -1,17 +1,20 @@
-exports.loginResponse = function(req, res, err, user, info) {
+exports.loginResponse = function (req, res, err, user, info) {
     var response, userSession;
-    if(err || !user) {
+    if (err || !user) {
         response = {
             code: 0
         };
-        if(info) {
+        if (info) {
             response.message = info.message;
         } else {
             response.message = 'Something wrong happened during the login';
         }
-        return res.redirect('/login?err=' + encodeURIComponent(JSON.stringify(response)));
+        if (req.is('html')) {
+            return res.redirect('/login?err=' + encodeURIComponent(JSON.stringify(response)));
+        }
+        res.send({error: err});
     } else {
-        if(!user.user_role.code) {
+        if (!user.user_role.code) {
             response = {};
             res.redirect('/login?err=' + encodeURIComponent(JSON.stringify(response)));
             return;
@@ -28,6 +31,9 @@ exports.loginResponse = function(req, res, err, user, info) {
         res.cookie('user', JSON.stringify({
             name: user.name
         }));
+        if (req.is('html')) {
+            res.redirect('/');
+        }
         res.send(user);
     }
 };
